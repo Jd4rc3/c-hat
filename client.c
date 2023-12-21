e5eaa7b7e8f1a5e243a3d1277f8dbeec959c8ba0
Juan Daniel <jdarce91@misena.edu.co>
2023-12-21 15:21:36 -0500

    Echo server con sigint

--- client.c
+++ client.c
@@ -1,47 +1,60 @@
-#include <string.h>
-#include <stdio.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
 #include <unistd.h>
+#define MAX_LENGTH 1000
+
+static volatile int keepRunning = 1;
+void intHandler(int dummy) { keepRunning = 0; }
 
 int main() {
-    int client_fd;
-    struct sockaddr_in server_address;
+  signal(SIGINT, intHandler);
 
-    client_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (client_fd == -1) {
-        perror("Error al crear el socket del cliente");
-        return 1;
-    }
+  int client_fd;
+  struct sockaddr_in server_address;
+  char buffer[MAX_LENGTH];
+  char recv_buffer[MAX_LENGTH];
 
-    server_address.sin_family = AF_INET;
-    server_address.sin_port = htons(8080);
-    if(inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr)<=0) {
-        printf("\nDirección inválida / Dirección no soportada \n");
-        return -1;
-    }
+  client_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (client_fd == -1) {
+    perror("Error al crear el socket del cliente");
+    return 1;
+  }
 
-    if (connect(client_fd, (struct sockaddr *)&server_address, sizeof(server_address)) < 0) {
-        perror("Error al conectar con el servidor");
-        return 1;
-    }
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons(8080);
+  if (inet_pton(AF_INET, "127.0.0.1", &server_address.sin_addr) <= 0) {
+    printf("\nDirección inválida / Dirección no soportada \n");
+    return -1;
+  }
 
-    char buffer[200] = "Hola, servidor!";
+  if (connect(client_fd, (struct sockaddr *)&server_address,
+              sizeof(server_address)) < 0) {
+    perror("Error al conectar con el servidor");
+    return 1;
+  }
+
+  while (keepRunning) {
+    fgets(buffer, 100 + 1, stdin);
+    buffer[strcspn(buffer, "\n")] = 0;
+    printf("Texto leido: %s\n", buffer);
+
     send(client_fd, buffer, strlen(buffer), 0);
 
-    char recv_buffer[200];
-    int bytesRead = read(client_fd, recv_buffer, 200);
+    int bytesRead = read(client_fd, recv_buffer, MAX_LENGTH);
     if (bytesRead > 0) {
-        recv_buffer[bytesRead] = '\0';
-        printf("Respuesta del servidor: %s\n", recv_buffer);
+      printf("Respuesta del servidor: %s\n", recv_buffer);
     } else if (bytesRead == 0) {
-        printf("La conexión se ha cerrado\n");
+      printf("La conexión se ha cerrado\n");
     } else {
-        perror("Error al leer del socket");
+      perror("Error al leer del socket");
     }
+  }
 
-    close(client_fd);
+  close(client_fd);
 
-    return 0;
+  return 0;
 }
--- main.c
+++ main.c
@@ -1,67 +1,74 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <signal.h>
 #include <stdio.h>
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <unistd.h>
-#include <arpa/inet.h>
+#define MAX_LENGTH 1000
 
-int main()
-{
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    socklen_t addr_size = sizeof(address);
+static volatile int keepRunning = 1;
+void intHandler(int dummy) { keepRunning = 0; }
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+int main() {
+  signal(SIGINT, intHandler);
 
-    if (server_fd == -1){
-        perror("Error al crear el socket del servidor");
-        return 1;
-    }
+  int server_fd, new_socket;
+  struct sockaddr_in address;
+  socklen_t addr_size = sizeof(address);
+  char bufferRead[MAX_LENGTH];
 
-    // Setup socket address and port
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(8080);
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Bind server address to socket
-    if (bind(server_fd, (struct sockaddr *)&address, addr_size) < 0){
-        perror("Error al obtener la direccion y el puerto del socket");
+  if (server_fd == -1) {
+    perror("Error al crear el socket del servidor");
+    return 1;
+  }
 
-        return 1;
-    }
+  // Setup socket address and port
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(8080);
 
-    printf("Direccion: %s\n", inet_ntoa(address.sin_addr));
-    printf("Puerto:  %d\n", ntohs(address.sin_port));
+  // Bind server address to socket
+  if (bind(server_fd, (struct sockaddr *)&address, addr_size) < 0) {
+    perror("Error al obtener la direccion y el puerto del socket");
 
-    // Listen incoming connections
-    if (listen(server_fd, 3) < 0){
-        perror("Error al escuchar conexiones");
-        return 1;
-    }
+    return 1;
+  }
 
-    printf("Escuchando en el puerto: %d\n", ntohs(address.sin_port));
+  printf("Direccion: %s\n", inet_ntoa(address.sin_addr));
+  printf("Puerto:  %d\n", ntohs(address.sin_port));
 
+  // Listen incoming connections
+  if (listen(server_fd, 3) < 0) {
+    perror("Error al escuchar conexiones");
+    return 1;
+  }
+
+  printf("Escuchando en el puerto: %d\n", ntohs(address.sin_port));
+
+  new_socket = accept(server_fd, (struct sockaddr *)&address, &addr_size);
+
+  if (new_socket < 0) {
+    perror("Error al aceptar la conexion");
+    return 1;
+  }
+
+  while (keepRunning) {
     // accept incoming connections
-    new_socket = accept(server_fd, (struct sockaddr *)&address, &addr_size);
-    if (new_socket < 0) {
-        perror("Error al aceptar la conexion");
-        return 1;
-    }
-
-    char bufferRead[200];
-    int bytesRead = read(new_socket, bufferRead, 200);
+    int bytesRead = read(new_socket, bufferRead, MAX_LENGTH);
     if (bytesRead > 0) {
-        bufferRead[bytesRead] = '\0'; // Ensure buffer ends up with null (string end)
-        printf("Bytes leídos: %s\n", bufferRead);
-
-        write(new_socket, bufferRead, 200);
+      write(new_socket, bufferRead, MAX_LENGTH);
     } else if (bytesRead == 0) {
-        printf("La conexión se ha cerrado\n");
+      printf("La conexión se ha cerrado\n");
+      intHandler(1);
     } else {
-        perror("Error al leer del socket");
+      perror("Error al leer del socket");
     }
+  }
 
-    close(new_socket);
-    close(server_fd);
+  close(new_socket);
+  close(server_fd);
 
-    return 0;
+  return 0;
 }
