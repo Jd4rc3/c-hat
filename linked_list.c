06c3bda810079f1d23903776c2f38bb8087337db
Juan Daniel <jdarce91@misena.edu.co>
2023-12-29 12:08:33 -0500

    poor linked list implementation

---
+++ linked_list.c
@@ -0,0 +1,46 @@
+#include "linked_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+Node *new_node(client_t *client) {
+  Node *new_node = (Node *)malloc(sizeof(Node));
+  new_node->client = client;
+  new_node->next = NULL;
+  return new_node;
+}
+
+void insert_node(Node **head, Node *new_node) {
+  if (*head == NULL) {
+    *head = new_node;
+    return;
+  }
+
+  Node *current = *head;
+  while (current->next != NULL) {
+    current = current->next;
+  }
+
+  current->next = new_node;
+}
+
+void delete_node(Node **head, Node *node) {
+  if (*head == NULL) {
+    return;
+  }
+
+  Node *current = *head;
+  if (current == node) {
+    *head = current->next;
+    free(current);
+    return;
+  }
+
+  while (current->next != NULL) {
+    if (current->next == node) {
+      current->next = current->next->next;
+      free(node);
+      return;
+    }
+    current = current->next;
+  }
+}
---
+++ linked_list.h
@@ -0,0 +1,52 @@
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+typedef struct {
+  int sockfd;
+  char *name;
+} client_t;
+
+typedef struct Node {
+  client_t *client;
+  struct Node *next;
+} Node;
+
+Node *new_node(client_t *client);
+
+void insert_node(Node **head, Node *new_node);
+
+void delete_node(Node **head, Node *node);
+
+#endif
+
+// TODO **Verificación de `malloc`**: En función `new_node` es una buena
+// práctica verificar si `malloc` ha devuelto `NULL`, lo que indicaría que la
+// asignación de memoria ha fallado. Por ejemplo:
+//
+//     ```c
+//     Node *new_node = (Node *)malloc(sizeof(Node));
+//     if (new_node == NULL) {
+//         fprintf(stderr, "Error al asignar memoria para el nuevo nodo.\n");
+//         return NULL;
+//     }
+//     ```
+//
+// TODO **Eliminar nodo en lista enlazada**: Al eliminar un nodo de la lista se
+// recorre la lista hasta encontrar el nodo anterior al nodo a eliminar. Esto
+// lleva tiempo O(n). Si no necesitas mantener un orden específico en tu lista,
+// podrías considerar eliminar el nodo al principio de la lista, lo que llevaría
+// tiempo constante O(1).
+//
+// TODO **Inserción en lista enlazada**: En tu función `insert_node`, estás
+// insertando nuevos nodos al final de la lista. Esto requiere recorrer toda la
+// lista, lo que lleva tiempo O(n). Si no necesitas mantener un orden específico
+// en tu lista, podrías considerar insertar al principio de la lista, lo que
+// llevaría tiempo constante O(1).
+//
+// TODO **Manejo de errores**: Considerar agregar más manejo de errores en tu
+// código. Por ejemplo, ¿qué debería hacer tu programa si se intenta eliminar un
+// nodo que no está en la lista?
+//
+// TODO **Limpieza de memoria**: Asegúrate de liberar toda la memoria asignada
+// con `malloc` cuando ya no es necesaria. Esto incluye los nodos de la lista
+// enlazada y las estructuras `client_t`.
